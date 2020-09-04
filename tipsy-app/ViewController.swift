7b12c962e1de38dfa135b5480a011f2aa1a5e385
Burak Engin <kburakengin@icloud.com>
2020-09-04 12:16:17 +0300

    Initial Commit

---
+++ tipsy-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipsy-app
+//
+//  Created by Burak Engin on 4.09.2020.
+//  Copyright © 2020 Burak Engin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
